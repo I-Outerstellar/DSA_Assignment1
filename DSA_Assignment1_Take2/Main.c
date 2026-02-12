90d7f956f0f0c17ab44c56f7e729f45bba3d60a6
Rajdeep Chowdhury <rajdeepchowdhury2007@gmail.com>
2026-02-12 18:06:11 -0500

    Wrote a majority of Main.c

--- DSA_Assignment1_Take2/Main.c
+++ DSA_Assignment1_Take2/Main.c
@@ -1,6 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "Main.h"
 #include "Queue.h"
 
 int main(int argc, char* argv[]) {
 
+}
+
+int random(int min, int max) {
+	if (min == max) return min;
+	if (max > min) {
+		printf("ERROR: Max cannot be over min.");
+		return -1;
+	}
+	return rand() % (max - min + 1) + min;
+}
+
+User generateUser() {
+	unsigned int level = random(1, 60);
+	Faction faction = random(1, 3);
+	char username[11];
+	for (int i = 0; i < 10; i++) {
+		int randomChance = random(1, 10);
+		if (randomChance == 1 && i > 3) {
+			username[i] = '\n';
+			break;
+		}
+		else switch (randomChance % 3) {
+		case 0:
+			username[i] = random(48, 57); //0-9
+			break;
+		case 1:
+			username[i] = random(65, 90); //A-Z
+			break;
+		default:
+			username[i] = random(97, 122); //a-z
+		}
+	}
+	username[10] = '\0';
+
+	return createUser(level, faction, username);
+}
+
+void generateQueue(Queue* queue) {
+	
 }
--- DSA_Assignment1_Take2/User.c
+++ DSA_Assignment1_Take2/User.c
@@ -1,1 +1,27 @@
 #include "User.h"
+#include <string.h>
+
+User createUser(unsigned int level, Faction faction, char* username) {
+	User user;
+	
+	//Clamp the level
+	level = (level < 1) ? 1 : level;
+	level = (level > 60) ? 60 : level;
+
+	//Create and return user
+	user.level = level;
+	user.faction = faction;
+	strcpy_s(user.username, 11, username);
+	user.username[10] = '\0';
+	return user;
+}
+
+const char* factionToString(Faction faction) {
+	//Return string based on switch statement
+	switch (faction) {
+	case RED: return "red";
+	case GREEN: return "green";
+	case BLUE: return "blue";
+	default: return "error/null";
+	}
+}
